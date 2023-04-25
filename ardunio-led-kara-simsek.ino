4334077c82772104c59c922b17f9c25e6febb31f
Buğra <109425576+bugraozdmr@users.noreply.github.com>
2023-04-25 20:59:31 +0300

    Update ardunio-led-kara-simsek.ino

--- ardunio-led-kara-simsek.ino
+++ ardunio-led-kara-simsek.ino
@@ -1,5 +1,5 @@
 int dizi[]={2,3,4,5};
-int a=0;
+int a=0,b;
 
 void setup() {
   Serial.begin(9600);
@@ -13,11 +13,22 @@ void loop() {
   //100.seferde dursun
   
   for(int i=0;i<4;i++){
+    if(b==dizi[i]) continue;
     Serial.println(a);
     digitalWrite(dizi[i],HIGH);
     delay(300);   //beklesin az
     a++;
     digitalWrite(dizi[i],LOW);
+    b=dizi[i];
+  }
+  for(int i=3;i>=0;i--){
+    if(b==dizi[i]) continue;  //yakti sondurdu bekleme olmadigi icin direkt tekrar yakti bunu boylece onledik
+    Serial.println(a);
+    digitalWrite(dizi[i],HIGH);
+    delay(300);   //beklesin az
+    a++;
+    digitalWrite(dizi[i],LOW);
+    b=dizi[i];
   }
   if(a>100) exit(0);
 }
